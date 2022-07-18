4ec7dccb0850104862b8b16f767cba3f99131d61
Anirudh Mukherjee <f20200970@goa.bits-pilani.ac.in>
2022-07-18 17:23:15 +0530

    Added support for metrics

--- jeta/opti_trainer.py
+++ jeta/opti_trainer.py
@@ -2,11 +2,11 @@ from functools import partial
 from typing import Callable, List, Tuple
 
 import jax
+import optax
 from flax import struct
 from flax.training import train_state
 from jax.numpy import ndarray
 
-import optax
 
 class MetaTrainState(train_state.TrainState):
     adapt_fn: Callable = struct.field(pytree_node=False)
@@ -33,12 +33,15 @@ class OptiTrainer:
         state = MetaTrainState.create(
             params=params, apply_fn=apply_fn, adapt_fn=adapt_fn, loss_fn=loss_fn, tx=tx
         )
-        return state.replace(opt_state=tx.init(params['params']))
+        return state.replace(opt_state=tx.init(params["params"]))
 
     @staticmethod
-    @jax.jit
-    # @partial(jax.jit, static_argnums=(2,))
-    def meta_train_step(state: MetaTrainState, tasks) -> Tuple[MetaTrainState, ndarray, List[ndarray]]:
+    @partial(jax.jit, static_argnums=(2,))
+    def meta_train_step(
+        state: MetaTrainState,
+        tasks,
+        metrics: List[Callable[[ndarray, ndarray], ndarray]] = [],
+    ) -> Tuple[MetaTrainState, ndarray, List[ndarray]]:
         """Performs a single meta-training step on a batch of tasks.
 
         The fuctions first adapts to the support set and then evaluates it's perfomance
@@ -55,33 +58,47 @@ class OptiTrainer:
 
         params = state.params
         theta = params["params"]
-        
-        def batch_meta_train_loss(theta, apply_fn, adapt_fn, loss_fn, tasks):
-            loss = jax.vmap(OptiTrainer.meta_loss, in_axes=(None, None, None, None, 0))(
-                params.copy({'params': theta}), apply_fn, adapt_fn, loss_fn, tasks
-            )
-            return loss.mean()#, [metric.mean() for metric in metrics_value]
 
-        loss, grads = jax.value_and_grad(batch_meta_train_loss)(
-            theta, state.apply_fn, state.adapt_fn, state.loss_fn, tasks
-        )
+        def batch_meta_train_loss(theta, apply_fn, adapt_fn, loss_fn, tasks):
+            loss, metrics_value = jax.vmap(
+                OptiTrainer.meta_loss, in_axes=(None, None, None, None, 0, None)
+            )(
+                params.copy({"params": theta}),
+                apply_fn,
+                adapt_fn,
+                loss_fn,
+                tasks,
+                metrics,
+            )
+            return loss.mean(), [metric.mean() for metric in metrics_value]
+
+        (loss, metrics_value), grads = jax.value_and_grad(
+            batch_meta_train_loss, has_aux=True
+        )(theta, state.apply_fn, state.adapt_fn, state.loss_fn, tasks)
         # state = state.apply_gradients(grads=grads)
 
         # if state.step == 0: # Initialize optimizer
         #     state = state.replace(opt_state=state.tx.init(state.params["params"]))
 
-        updates, new_opt_state = state.tx.update(grads, state.opt_state, state.params['params'])
-        new_params = optax.apply_updates(state.params['params'], updates)
-        params = state.params.copy({'params': new_params})
+        updates, new_opt_state = state.tx.update(
+            grads, state.opt_state, state.params["params"]
+        )
+        new_params = optax.apply_updates(state.params["params"], updates)
+        params = state.params.copy({"params": new_params})
 
-        state = state.replace(step=state.step+1, params=params, opt_state=new_opt_state)
+        state = state.replace(
+            step=state.step + 1, params=params, opt_state=new_opt_state
+        )
 
-        return state, loss
+        return state, loss, metrics_value
 
     @staticmethod
-    @jax.jit
-    # @partial(jax.jit, static_argnums=(2,))
-    def meta_test_step(state: MetaTrainState, tasks) -> Tuple[ndarray, List[ndarray]]:
+    @partial(jax.jit, static_argnums=(2,))
+    def meta_test_step(
+        state: MetaTrainState,
+        tasks,
+        metrics: List[Callable[[ndarray, ndarray], ndarray]] = [],
+    ) -> Tuple[ndarray, List[ndarray]]:
         """Performs a single meta-testing step on a batch of tasks.
 
         The function first adapts to the support set and then evaluates it's perfomance
@@ -95,19 +112,20 @@ class OptiTrainer:
         Returns:
             Tuple[jnp.ndarray, List[jnp.ndarray]: (Loss, metrics).
         """
-        # , metrics: List[Callable[[ndarray, ndarray], ndarray]]=[]
 
         params = state.params
         apply_fn = state.apply_fn
         loss_fn = state.loss_fn
         adapt_fn = state.adapt_fn
-        loss = jax.vmap(OptiTrainer.meta_loss, in_axes=(None, None, None, None, 0))(
-            params, apply_fn, adapt_fn, loss_fn, tasks
-        )
-        return loss.mean()#, [metric.mean() for metric in metrics_value]
+        loss, metrics_value = jax.vmap(
+            OptiTrainer.meta_loss, in_axes=(None, None, None, None, 0, None)
+        )(params, apply_fn, adapt_fn, loss_fn, tasks, metrics)
+        return loss.mean(), [metric.mean() for metric in metrics_value]
 
     @staticmethod
-    def meta_loss(params, apply_fn, adapt_fn, loss_fn, task) -> Tuple[ndarray, List[ndarray]]:
+    def meta_loss(
+        params, apply_fn, adapt_fn, loss_fn, task, metrics
+    ) -> Tuple[ndarray, List[ndarray]]:
         """Calculates the Meta Loss of a task
 
         Args:
@@ -128,9 +146,9 @@ class OptiTrainer:
 
         # Evaluation step
         x_train, y_train = query_set
-        logits = apply_fn(params.copy({'params': theta}), x_train, train=False)
+        logits = apply_fn(params.copy({"params": theta}), x_train, train=False)
 
         # Calculate metrics
-        # metrics_value = [metric(logits, y_train) for metric in metrics]
+        metrics_value = [metric(logits, y_train) for metric in metrics]
 
-        return loss_fn(logits, y_train)
+        return loss_fn(logits, y_train), metrics_value
