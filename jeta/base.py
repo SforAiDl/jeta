3a2bc040510e50af2a629bbebb479ba0de925969
arjun <arjunputhli00@gmail.com>
2022-07-25 18:24:50 +0530

    added docstring for base.py

--- jeta/base.py
+++ jeta/base.py
@@ -4,6 +4,17 @@ from jax import random
 
 
 class BaseLearner(ABC):
+    """Base Learner class
+
+    Attributes:
+        batch_size(int): batch size for training the algorithm on
+        alpha(float): Learning rate
+        fas(int): Fast adaptation steps
+        seed(int): integer value for generating key for random generators
+        first_order(bool): True/False, for extending MAML to FO-MAML
+
+    """
+
     def __init__(
         self, batch_size: int = 32, alpha: float = 0.001, fas: int = 1, **kwargs
     ):
@@ -19,11 +30,38 @@ class BaseLearner(ABC):
         if self.seed is not None:
             self.key = random.PRNGKey(self.seed)
 
-    def train(self):
+    def train(self, params, batch):
+        """train algorithm
+
+        Args:
+            params(ndarray): initial parameters for the model
+            batch(ndarray): training batch for the algorithm
+
+        Return:
+            new_params(ndarray): updated parameters after 1 epoch
+        """
         raise NotImplementedError
 
-    def loss(self, theta, batch):
+    def loss(self, params, batch):
+        """loss with current parameters
+
+        Args:
+            params(ndarray): current set of parameters
+            batch(ndarray): training batch for the algorithm
+
+        Return:
+            loss(float): loss incurred
+        """
         raise NotImplementedError
 
-    def test(self):
+    def test(self, params, batch):
+        """test algorithm
+
+        Args:
+            params(ndarray): final set of parameters after training
+            batch(ndarray): testing batch of data
+
+        Return:
+            output(ndarray): predicted output
+        """
         raise NotImplementedError
